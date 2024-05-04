24199958e2f782f67a70433344843d821c080636
Alex <hulap.lapshin@yandex.ru>
2024-05-04 13:28:36 +0300

    Внесены изменения в исходный файл для ветки copy

--- Furniture_store.cs
+++ Furniture_store.cs
@@ -11,7 +11,7 @@ class FurnitureStore
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
         List<Task> tasks = new List<Task>();
-        for (int i = 0; i < 10000; i++)
+        for (int i = 0; i < 100; i++)
         {
             tasks.Add(Task.Run(() => PlaceOrder("стол", 1)));
         }
